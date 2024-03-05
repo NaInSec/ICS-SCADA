3557650945538e8a88fba60f64cbd9f06dcccd90
NaInSec <nainsec17081945@gmail.com>
2024-03-05 17:22:50 +0700

    ICS-SCADA

---
+++ RTU/LdKombi.bat
@@ -0,0 +1,2 @@
+f206_ld.exe komb305.dsk -1 -9600
+pause
---
+++ list.sh
@@ -0,0 +1,2 @@
+wget https://downloads.enercon.de/scada/enercon-scada-remote-3-13-4-setup.msi && wget https://dl.liangroup.net/SANS/SANS%20-%20ICS410%20-%20ICS%20SCADA%20Security%20Essentials/Text/2016/ICS410%20-%20ICS%20SCADA%20Security%20Essentials.rar && wget https://phobos.martem.ee/shr/SCADA-docs/commercial/Sreenshot/SilPower-le/SCADA%20screenshots.zip
+
